13ccfaf751c623e3b5dabd7a3b82fef5cc89d8e7
SAMTECHNET <samlaja1292@gmail.com>
2022-01-12 00:40:24 +0100

    Initial Commit

---
+++ greet.js
@@ -0,0 +1,13 @@
+let myDate = new Date();
+    let hrs = myDate.getHours();
+
+    let greet;
+
+    if (hrs < 12)
+        greet = 'Good Morning';
+    else if (hrs >= 12 && hrs <= 17)
+        greet = 'Good Afternoon';
+    else if (hrs >= 17 && hrs <= 24)
+        greet = 'Good Evening';
+    document.getElementById('greetings').innerHTML =
+        '<b>' + greet + ',' + '</b>  samuel ';
